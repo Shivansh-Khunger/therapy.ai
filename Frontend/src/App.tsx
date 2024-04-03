e5c7d13513c1447603e1bc9eb4a73fc9c2318c96
Shivansh-Khunger <khunger.shivansh@outlook.com>
2024-04-03 11:37:27 +0530

    add auto send & recieve

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -6,37 +6,84 @@ import b64toBlob from "b64-to-blob";
 function App() {
   const webCamRef = useRef<Webcam>(null);
 
+  // Ignore this code left only for if needed in future
+
+  // const useRecievedFrame = (initialValue: string | null) => {
+  //   const [recievedFrame, setRecievedFrame] = useState<string | null>(
+  //     initialValue,
+  //   );
+
+  //   let current = recievedFrame;
+
+  //   const get = () => current;
+
+  //   const set = (newValue: string) => {
+  //     current = newValue;
+  //     setRecievedFrame(newValue);
+  //     return current;
+  //   };
+
+  //   return {
+  //     get,
+  //     set,
+  //   };
+  // };
+
   const [imgSrc, setImgSrc] = useState<string | null>(null);
   const [socketUrl, setSocketUrl] = useState("ws://127.0.0.1:9080/ws");
   const [messageHistory, setMessageHistory] = useState<MessageEvent<any>[]>([]);
   const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl, {});
+  const [start, setStart] = useState(false);
+  const [recievedFrame, setRecievedFrame] = useState<string | null>(null);
 
-  const capture = useCallback(() => {
+  // Ignore this code left only for if needed in future
+  // const rf = useRecievedFrame(null);
+
+  const capture = () => {
     const imageSrc = webCamRef.current?.getScreenshot();
+
     if (imageSrc) {
       setImgSrc(imageSrc);
     } else {
       console.log(`imageSrc is not assignable`);
     }
-  }, [webCamRef]);
+    return imageSrc;
+  };
 
-  const handleClickSendMessage = useCallback(async () => {
-    if (imgSrc) {
-      let imageSrcParts = imgSrc.split(",");
+  const handleClickSendMessage = (imageSrc: string | null) => {
+    if (imageSrc) {
+      let imageSrcParts = imageSrc.split(",");
       let base64Data = imageSrcParts[1];
       console.log(base64Data);
-      const blob = b64toBlob(base64Data, "image/jpeg");
-
-      console.log(blob);
-      sendMessage(blob);
+      sendMessage(base64Data);
     } else {
-      console.log(imgSrc);
+      console.log(imageSrc);
     }
-  }, [imgSrc]);
+  };
 
   const retake = () => {
     setImgSrc(null);
   };
+
+  useEffect(() => {
+    if (start) {
+      console.log("Here");
+      const tempInterval = setInterval(() => {
+        const capturedBase64 = capture();
+        handleClickSendMessage(capturedBase64 ? capturedBase64 : null);
+      }, 100);
+
+      return () => {
+        clearInterval(tempInterval);
+      };
+    } else {
+      console.log("Currently Stopped");
+    }
+  }, [start]);
+
+  useEffect(() => {
+    setRecievedFrame(lastMessage?.data);
+  }, [lastMessage]);
 
   const connectionStatus = {
     [ReadyState.CONNECTING]: "Connecting",
@@ -54,19 +101,37 @@ function App() {
     <>
       <div className="flex h-screen w-screen items-center justify-center gap-4">
         <div className="flex flex-col items-center justify-center">
+          <div className="flex justify-center gap-4">
+            {" "}
+            <button
+              className="border-4 border-black p-2"
+              onClick={() => {
+                setStart(true);
+              }}
+            >
+              Start
+            </button>
+            <button
+              className="border-4 border-black p-2"
+              onClick={() => {
+                setStart(false);
+              }}
+            >
+              Stop
+            </button>
+          </div>
           <h2 className="flex justify-center font-bold">Our Webcam</h2>
-          <Webcam ref={webCamRef} screenshotFormat="image/jpeg" />
+          <Webcam
+            ref={webCamRef}
+            screenshotFormat="image/jpeg"
+            mirrored={true}
+          />
         </div>
-
-        <button onClick={capture} className="absolute top-32">
-          Capture
-        </button>
-        <button
-          className="absolute left-4 top-32"
-          onClick={handleClickSendMessage}
-        >
-          Send
-        </button>
+        <div>
+          {recievedFrame ? (
+            <img src={`data:image/jpeg;base64,${recievedFrame}`} alt="" />
+          ) : null}
+        </div>
       </div>
     </>
   );
